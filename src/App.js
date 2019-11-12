508f630edb1a5cc4b04c9197b9d3c198d3791b25
Joe Kinney <joekinney53@gmail.com>
2019-11-11 18:40:55 -0600

    added skeleton API for recipes

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import Recipe from "./Components/Recipe";
+import "./App.css";
+
+const App = () => {
+  const APP_ID1 = "54d02822";
+  const APP_KEY1 = "bbf22a931c3d73de4a820e5086a670a5";
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID1}&app_key=${APP_KEY1}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
+  return (
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      {recipes.map(recipe => (
+        <Recipe
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default App;
